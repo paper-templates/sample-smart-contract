0a1a72f204c531f956d00a4e2600c960124b96ae
ElasticBottle <winstonyeo99@yahoo.com>
2022-05-27 15:37:16 -0700

    Remove whitelist from test

--- test/index.ts
+++ test/index.ts
@@ -3,8 +3,6 @@ import chai, { expect } from "chai";
 import ChaiAsPromised from "chai-as-promised";
 import { BigNumber, utils } from "ethers";
 import { ethers } from "hardhat";
-import keccak256 from "keccak256";
-import { MerkleTree } from "merkletreejs";
 import CollectionConfig from "../config/CollectionConfig";
 import ContractArguments from "../config/ContractArguments";
 import { NftContractType } from "../lib/NftContractProvider";
@@ -16,27 +14,6 @@ enum SaleType {
   PRE_SALE = CollectionConfig.preSale.price,
   PUBLIC_SALE = CollectionConfig.publicSale.price,
 }
-
-const whitelistAddresses = [
-  // Hardhat test addresses...
-  "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
-  "0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65",
-  "0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc",
-  "0x976EA74026E726554dB657fA54763abd0C3a0aa9",
-  "0x14dC79964da2C08b23698B3D3cc7Ca32193d9955",
-  "0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f",
-  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720",
-  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
-  "0x71bE63f3384f5fb98995898A86B02Fb2426c5788",
-  "0xFABB0ac9d68B0B445fB7357272Ff202C5651694a",
-  "0x1CBd3b2770909D4e10f157cABC84C7264073C9Ec",
-  "0xdF3e18d64BC6A983f673Ab319CCaE4f1a57C7097",
-  "0xcd3B766CCDd6AE721141F452C550Ca635964ce71",
-  "0x2546BcD3c84621e976D8185a91A922aE77ECEc30",
-  "0xbDA5747bFD65F08deb54cb465eB87D40e51B197E",
-  "0xdD2FD4581271e230360230F9337D5c0430Bf44C0",
-  "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199",
-];
 
 function getPrice(saleType: SaleType, mintAmount: number) {
   return utils.parseEther(saleType.toString()).mul(mintAmount);
@@ -147,7 +124,6 @@ describe(CollectionConfig.contractName, function () {
     );
 
     expect(await contract.paused()).to.equal(true);
-    expect(await contract.whitelistMintEnabled()).to.equal(false);
     expect(await contract.revealed()).to.equal(false);
 
     await expect(contract.tokenURI(1)).to.be.revertedWith(
@@ -162,31 +138,17 @@ describe(CollectionConfig.contractName, function () {
         .connect(whitelistedUser)
         .mint(1, { value: getPrice(SaleType.WHITELIST, 1) })
     ).to.be.revertedWith("The contract is paused!");
-    await expect(
-      contract
-        .connect(whitelistedUser)
-        .whitelistMint(1, [], { value: getPrice(SaleType.WHITELIST, 1) })
-    ).to.be.revertedWith("The whitelist sale is not enabled!");
-    await expect(
+    expect(
       contract
         .connect(holder)
         .mint(1, { value: getPrice(SaleType.WHITELIST, 1) })
     ).to.be.revertedWith("The contract is paused!");
-    await expect(
-      contract
-        .connect(holder)
-        .whitelistMint(1, [], { value: getPrice(SaleType.WHITELIST, 1) })
-    ).to.be.revertedWith("The whitelist sale is not enabled!");
+
     await expect(
       contract
         .connect(owner)
         .mint(1, { value: getPrice(SaleType.WHITELIST, 1) })
     ).to.be.revertedWith("The contract is paused!");
-    await expect(
-      contract
-        .connect(owner)
-        .whitelistMint(1, [], { value: getPrice(SaleType.WHITELIST, 1) })
-    ).to.be.revertedWith("The whitelist sale is not enabled!");
 
     // The owner should always be able to run mintForAddress
     await (await contract.mintForAddress(1, await owner.getAddress())).wait();
@@ -206,109 +168,6 @@ describe(CollectionConfig.contractName, function () {
     expect(
       await contract.balanceOf(await whitelistedUser.getAddress())
     ).to.equal(1);
-    expect(await contract.balanceOf(await holder.getAddress())).to.equal(0);
-    expect(await contract.balanceOf(await externalUser.getAddress())).to.equal(
-      0
-    );
-  });
-
-  it("Whitelist sale", async function () {
-    // Build MerkleTree
-    const leafNodes = whitelistAddresses.map((addr) => keccak256(addr));
-    const merkleTree = new MerkleTree(leafNodes, keccak256, {
-      sortPairs: true,
-    });
-    const rootHash = merkleTree.getRoot();
-    // Update the root hash
-    await (
-      await contract.setMerkleRoot("0x" + rootHash.toString("hex"))
-    ).wait();
-
-    await contract.setWhitelistMintEnabled(true);
-
-    await contract
-      .connect(whitelistedUser)
-      .whitelistMint(
-        1,
-        merkleTree.getHexProof(keccak256(await whitelistedUser.getAddress())),
-        { value: getPrice(SaleType.WHITELIST, 1) }
-      );
-    // Trying to mint twice
-    await expect(
-      contract
-        .connect(whitelistedUser)
-        .whitelistMint(
-          1,
-          merkleTree.getHexProof(keccak256(await whitelistedUser.getAddress())),
-          { value: getPrice(SaleType.WHITELIST, 1) }
-        )
-    ).to.be.revertedWith("Address already claimed!");
-    // Sending an invalid mint amount
-    await expect(
-      contract
-        .connect(whitelistedUser)
-        .whitelistMint(
-          await (await contract.maxMintAmountPerTx()).add(1),
-          merkleTree.getHexProof(keccak256(await whitelistedUser.getAddress())),
-          {
-            value: getPrice(
-              SaleType.WHITELIST,
-              await (await contract.maxMintAmountPerTx()).add(1).toNumber()
-            ),
-          }
-        )
-    ).to.be.revertedWith("Invalid mint amount!");
-    // Sending insufficient funds
-    await expect(
-      contract
-        .connect(whitelistedUser)
-        .whitelistMint(
-          1,
-          merkleTree.getHexProof(keccak256(await whitelistedUser.getAddress())),
-          { value: getPrice(SaleType.WHITELIST, 1).sub(1) }
-        )
-    ).to.be.rejectedWith(
-      Error,
-      "insufficient funds for intrinsic transaction cost"
-    );
-    // Pretending to be someone else
-    await expect(
-      contract
-        .connect(holder)
-        .whitelistMint(
-          1,
-          merkleTree.getHexProof(keccak256(await whitelistedUser.getAddress())),
-          { value: getPrice(SaleType.WHITELIST, 1) }
-        )
-    ).to.be.revertedWith("Invalid proof!");
-    // Sending an invalid proof
-    await expect(
-      contract
-        .connect(holder)
-        .whitelistMint(
-          1,
-          merkleTree.getHexProof(keccak256(await holder.getAddress())),
-          { value: getPrice(SaleType.WHITELIST, 1) }
-        )
-    ).to.be.revertedWith("Invalid proof!");
-    // Sending no proof at all
-    await expect(
-      contract
-        .connect(holder)
-        .whitelistMint(1, [], { value: getPrice(SaleType.WHITELIST, 1) })
-    ).to.be.revertedWith("Invalid proof!");
-
-    // Pause whitelist sale
-    await contract.setWhitelistMintEnabled(false);
-    await contract.setCost(
-      utils.parseEther(CollectionConfig.preSale.price.toString())
-    );
-
-    // Check balances
-    expect(await contract.balanceOf(await owner.getAddress())).to.equal(1);
-    expect(
-      await contract.balanceOf(await whitelistedUser.getAddress())
-    ).to.equal(2);
     expect(await contract.balanceOf(await holder.getAddress())).to.equal(0);
     expect(await contract.balanceOf(await externalUser.getAddress())).to.equal(
       0
@@ -346,15 +205,6 @@ describe(CollectionConfig.contractName, function () {
           ),
         })
     ).to.be.revertedWith("Invalid mint amount!");
-    // Sending a whitelist mint transaction
-    await expect(
-      contract
-        .connect(whitelistedUser)
-        .whitelistMint(1, [], { value: getPrice(SaleType.WHITELIST, 1) })
-    ).to.be.rejectedWith(
-      Error,
-      "insufficient funds for intrinsic transaction cost"
-    );
 
     // Pause pre-sale
     await contract.setPaused(true);
@@ -389,16 +239,6 @@ describe(CollectionConfig.contractName, function () {
     ).to.be.revertedWith("Ownable: caller is not the owner");
     await expect(
       contract.connect(externalUser).setPaused(false)
-    ).to.be.revertedWith("Ownable: caller is not the owner");
-    await expect(
-      contract
-        .connect(externalUser)
-        .setMerkleRoot(
-          "0x0000000000000000000000000000000000000000000000000000000000000000"
-        )
-    ).to.be.revertedWith("Ownable: caller is not the owner");
-    await expect(
-      contract.connect(externalUser).setWhitelistMintEnabled(false)
     ).to.be.revertedWith("Ownable: caller is not the owner");
     await expect(contract.connect(externalUser).withdraw()).to.be.revertedWith(
       "Ownable: caller is not the owner"
